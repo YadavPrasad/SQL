28342bec87defd989966750a23839efa7b4b441b
YadavPrasad <141987752+YadavPrasad@users.noreply.github.com>
2024-12-07 11:57:19 +0530

    Delete LeetCode directory

--- LeetCode/Single Number.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE L619
-USE L619
-
-CREATE TABLE MyNumbers (num INT);
-INSERT INTO MyNumbers  VALUES (8),(8),(3),(3),(1),(4),(5),(6);
-
---Query for Single number
-SELECT max(num) AS num 
-FROM (SELECT * FROM mynumbers GROUP BY num HAVING count(*)=1) AS singleno;
